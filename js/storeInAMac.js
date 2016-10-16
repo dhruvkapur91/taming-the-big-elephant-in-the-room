225f7f5534e3a46b12e02a6cb0231bc5d9d2ecb8
Dhruv Kapur <dhruvk@thoughtworks.com>
2016-10-16 15:24:44 +0530

    Adhocly added a lot of stuff

--- js/storeInAMac.js
+++ js/storeInAMac.js
@@ -26,8 +26,12 @@ $(function () {
 	    if(this.numberOfImages > 21000) {
 		console.log("Here")
 		this.tooMuchMessage = "But we can't store more than 21,000 images"
+		var alpha = Math.min(0.4,(this.numberOfImages - 21000)/1000)
+		$(".slide-background .present").css("background-color","rgba(255,0,0,"+alpha+")");
+		
 	    }else {
 		this.tooMuchMessage = ""
+		$(".slide-background .present").css("background-color","rgba(255,0,0,0)");
 	    }
         }
     };
