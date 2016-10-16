8fae4818af34054a79bc334274f4fdf393754831
Dhruv Kapur <dhruvk@thoughtworks.com>
2016-10-16 22:28:39 +0530

    Add stuff for scaling up...

--- js/scaleUp.js
+++ js/scaleUp.js
@@ -9,7 +9,8 @@ $(function () {
 	    this.tooMuchMessage = "";
          },
         update: function () {
-            this.totalSize = this.numberOfImages * this.sizePerImage;
+	    var actualSize = this.numberOfImages * this.sizePerImage;
+            this.totalSize = actualSize
 	    if(this.numberOfImages > 1) {
 		this.description = "images"
 	    } else {
@@ -21,17 +22,41 @@ $(function () {
 	    }else{
 		this.dataUnits = "MBs"
 	    }
-	    $(".spaceLeft")[0].innerHTML = Math.round(100 - (this.numberOfImages*100/21000)) + "%"
+	    if(this.totalSize > 1024){
+		this.totalSize = Math.round(this.totalSize * 100 / (1024))/100
+		this.dataUnits = "TBs"
+	    }
 	    $(".imageSize").attr("width",0.0115*this.numberOfImages)
-	    if(this.numberOfImages > 21000) {
-		console.log("Here")
-		this.tooMuchMessage = "But we can't store more than 21,000 images"
-		var alpha = Math.min(0.4,(this.numberOfImages - 21000)/1000)
+	    if(this.numberOfImages > 21000 && this.numberOfImages < 260000){
+		var factor = 0.00023;
+		var factorFill = factor*0.8;
+		console.log($(".imageSize").attr("width"))
+		$(".imageSize").attr("width",241 + factorFill*this.numberOfImages);
+		$(".slide-background .present").css("background-color","rgba(255,0,0,0)");		
+		$("#scale_up_limit .laptopSize").attr("width",250 + (this.numberOfImages-21000.0)*factor);
+		$("#scale_up_limit .laptopSize").attr("height",156 + (this.numberOfImages-21000.0)*factor);
+		$(".imageSize").attr("height",146 + factorFill*this.numberOfImages);
+		$("#scale_up_limit .appleLogo").attr("x",155 + (this.numberOfImages-21000.0)*factor*0.6);
+		$("#scale_up_limit .appleLogo").attr("y",150 + (this.numberOfImages-21000.0)*factor*0.6);
+		$("#scale_up_limit svg text")[0].innerHTML = "Buying more disk..."
+		this.tooMuchMessage = "";
+	    } else if(this.numberOfImages >= 260000){
+//		console.log("Not here!!")
+		var alpha = Math.min(0.4,(this.numberOfImages - 260000)/1000)
 		$(".slide-background .present").css("background-color","rgba(255,0,0,"+alpha+")");
-		
-	    }else {
-		this.tooMuchMessage = ""
+		this.tooMuchMessage = "Sadly limited on how much memory/disk we can have... <br/> Both physically and economically";
+		$("#scale_up_limit svg text")[0].innerHTML = "No more. Too costly!"
+		$("#scale_up_limit .imageSize").attr("height",200);		
+		$("#scale_up_limit .imageSize").attr("width",295);		
+	    } else{
+//		console.log($(".imageSize").attr("width"))		
+		$("#scale_up_limit .laptopSize").attr("width",250);
+		$("#scale_up_limit .laptopSize").attr("height",156);
+		$("#scale_up_limit .imageSize").attr("height",146);
+		$("#scale_up_limit .appleLogo").attr("x",155);
+		$("#scale_up_limit .appleLogo").attr("y",150);
 		$(".slide-background .present").css("background-color","rgba(255,0,0,0)");
+		this.tooMuchMessage = "";
 	    }
         }
     };
